942243ecba40aeffb7ed1f5f7975af62496b93b3
OD-T14S-25\PCUser <od@enso-managers.de>
2025-11-17 16:11:09 +0100

    restructure and complement tests pig-metaclasses.spec.ts

--- tests/unit/pig-metaclasses.spec.ts
+++ tests/unit/pig-metaclasses.spec.ts
@@ -119,11 +119,10 @@ describe("PIG Metaclasses", () => {
         
     });
 
-    test("Property class setup", () => {
-        // NOTE: Property needs to reference PropertClass but there is no validation in either class to ensure that
-        // either of them exists.
+    test("Test class pig:Propery", () => {
         let test_PC = new Property(propertyClass_input);
 
+        // check the attribute values:
         expect(test_PC.id).toBe("dcterms:type");
         expect(test_PC.title).toEqual({ text: "The type or category", lang: "en" });
         expect(test_PC.description).toEqual({ text: "This is a class for a property named dcterms:type for use by anEntity or aRelationship", lang: "en" });
@@ -135,17 +134,28 @@ describe("PIG Metaclasses", () => {
         expect(test_PC.maxLength).toBe(20);
         expect(test_PC.defaultValue).toBe("default_category");
 
+        // check the output:
+        let entityClass_output = test_PC.get();
+        expect(propertyClass_output).toEqual(Object.assign({ itemType: "pig:Property" }, propertyClass_input)); // itemType is added at object creation
+
+    });
+
+    test("Test instance pig:aProperty", () => {
+        // NOTE: aProperty needs to reference PropertClass but there is no validation in either class to ensure that
+        // either of them exists.
         let test_P = new AProperty(property_input);
 
+        // check the attribute values:
         expect(test_P.itemType).toBe(PigItemType.aProperty);
         expect(test_P.hasClass).toBe('dcterms:type');
         expect(test_P.value).toBe("A category");  // usually a property belongs to a certain entity or relationship
 
     });
 
-    test("Entity class setup", () => {
+    test("Test class pig:Entity", () => {
         let test_EC = new Entity(entityClass_input);
 
+        // check the attribute values:
         expect(test_EC.id).toBe('o:entityClass_1');
         expect(test_EC.title).toEqual({ text: 'Title of Entity Class 1', lang: "en" });
         expect(test_EC.description).toEqual({ text: 'Description of o:entityClass_1', lang: "en" });
@@ -153,17 +163,18 @@ describe("PIG Metaclasses", () => {
         expect(test_EC.itemType).toBe(PigItemType.Entity);
         expect(test_EC.eligibleProperty).toStrictEqual(["dcterms:type"]);
 
+        // check the output:
         let entityClass_output = test_EC.get();
-
-        expect(entityClass_output).toEqual(Object.assign({itemType: "pig:Entity"}, entityClass_input));
+        expect(entityClass_output).toEqual(Object.assign({ itemType: "pig:Entity" }, entityClass_input)); // itemType is added at object creation
 
     });
 
-    test("Relationship class setup", () => {
+    test("Test class pig:aRelationship", () => {
         // NOTE: Relationship needs to reference two Entity objects but there is no validation in either class to ensure that
         // either of them exists.
         let test_RC = new Relationship(relationshipClass_input);
 
+        // check the attribute values:
         expect(test_RC.id).toBe('o:relationshipClass');
         expect(test_RC.title).toEqual({ text: 'Title of RelationshipClass', lang: "en" });
         expect(test_RC.description).toEqual({ text: 'Description of o:relationshipClass', lang: "en" });
@@ -172,6 +183,12 @@ describe("PIG Metaclasses", () => {
         expect(test_RC.eligibleTarget).toStrictEqual(['o:entityClass_1']);
         expect(test_RC.eligibleSource).toStrictEqual(['o:entityClass_1']);
         expect(test_RC.eligibleProperty).toStrictEqual(["dcterms:type"]);
+
+
+        // check the output:
+        let relationshipClass_output = test_RC.get();
+        expect(relationshipClass_output).toEqual(Object.assign({ itemType: "pig:Relation" }, relationshipClass_input)); // itemType is added at object creation
+
     });
 
     /* ToDo: ... more tests to come */
