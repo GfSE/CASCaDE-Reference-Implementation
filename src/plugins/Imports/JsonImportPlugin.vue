358ba26442ae7836f48633304fea7d6a15074956
OD-T14S-25\PCUser <od@enso-managers.de>
2025-12-12 13:57:59 +0100

    corrections to separated jsonld and reqif import plugins.

--- src/plugins/Imports/JsonImportPlugin.vue
+++ src/plugins/Imports/JsonImportPlugin.vue
@@ -1,13 +1,13 @@
 <template>
-    <v-btn color='primary' @click='dialog = true'>Import REQIF</v-btn>
+    <v-btn color='primary' @click='dialog = true'>Import JSON-LD</v-btn>
     <v-dialog v-model='dialog'>
         <v-card class='w-50'>
             <v-card-title>Select Import Files</v-card-title>
             <v-card-actions>
                 <v-file-input
                     v-model='selectedFiles'
-                    accept='.reqif'
-                    label='REQIF Input'
+                    accept='.jsonld'
+                    label='JSON-LD Input'
                     prepend-icon='mdi-folder-open'
                     multiple
                 ></v-file-input>
@@ -20,7 +20,7 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import { reqif2pig } from '../../utils/import/ReqIF/reqif2pig';
+// import { jsonld2pig } from '../../utils/import/jsonld/jsonld2pig';
 
 @Options({
   data() {
@@ -31,14 +31,16 @@ import { reqif2pig } from '../../utils/import/ReqIF/reqif2pig';
   },
   methods: {
     submitFiles() {
-        const ReqifTranslator = new reqif2pig();
-        let translatorResponse = ReqifTranslator.toPig(this.selectedFiles);
+/*
+        const JsonldTranslator = new jsonld2pig();
+        let translatorResponse = JsonldTranslator.toPig(this.selectedFiles);
         console.log(translatorResponse.ok);
         console.log(translatorResponse.status);
 
         // reset variables
         this.dialog = false;
         this.selectedFiles = [];
+*/
     }
   },
 })
