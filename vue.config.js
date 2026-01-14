96413276237f932e290d57e3e0b23f461c6f659e
Chris Saenz <chrissaenz@psg-inc.net>
2026-01-14 11:38:18 -0700

    updated dev server content policy to fix broken interfaces page

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,52 @@
 const { defineConfig } = require('@vue/cli-service')
 const Components = require('unplugin-vue-components/webpack')
 
-//module.exports = defineConfig({
-//  transpileDependencies: true
-//})
-
-// This alias allows for runtime compilation of components
 module.exports = {
-    configureWebpack: {
-		resolve: {
-		  alias: {
-			'vue$': 'vue/dist/vue.esm-bundler.js',
-		  },
-		},
-		plugins: [
-			Components({
-				dirs: ["src/components"],
-				extensions: ["vue"],
-				globs: ["src/components/*.vue"],
-				dts: true
-			})
-		]
-	},
-    publicPath: './'
-};
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+    plugins: [
+      require('unplugin-vue-components/webpack')({
+        dirs: ['src/components'],
+        extensions: ['vue'],
+        globs: ['src/components/*.vue'],
+        dts: true,
+      }),
+    ],
+  },
+
+  devServer: {
+    headers: {
+      'Content-Security-Policy': [
+        "default-src 'none'",
+        "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+        "style-src 'self' 'unsafe-inline'",
+        "img-src 'self' data:",
+        "font-src 'self'",
+        "connect-src 'self' ws: http: https:",
+        "base-uri 'self'",
+        "object-src 'none'",
+      ].join('; '),
+    },
+
+    historyApiFallback: {
+      headers: {
+        'Content-Security-Policy': [
+          "default-src 'none'",
+          "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+          "style-src 'self' 'unsafe-inline'",
+          "img-src 'self' data:",
+          "font-src 'self'",
+          "connect-src 'self' ws: http: https:",
+          "base-uri 'self'",
+          "object-src 'none'",
+        ].join('; '),
+      },
+    },
+  },
+
+  publicPath: './',
+}
